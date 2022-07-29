13b58f0d2e2a2226951c7fcd3ba27e8b070e59b3
carolyn.sher <carolyn.sher@wgu.edu>
2022-07-29 14:55:53 -0400

    first commit

---
+++ src/main/java/edy/wgu/d387_jar/D387JarApplication.java
@@ -0,0 +1,13 @@
+package edy.wgu.d387_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D387JarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D387JarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edy/wgu/d387_jar/D387JarApplicationTests.java
@@ -0,0 +1,13 @@
+package edy.wgu.d387_jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D387JarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
